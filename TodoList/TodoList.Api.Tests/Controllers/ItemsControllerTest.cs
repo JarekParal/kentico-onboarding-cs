27d3c25e610562bbfc2188cbcef2e2a8fa1b92ef
JarekParal <jaroslavp@kentico.com>
2018-09-17 16:30:51 +0200

    KA-381 Add test on Get() in ItemsController

---
+++ TodoList/TodoList.Api.Tests/Controllers/ItemsControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using TodoList.Api.Controllers;
+using TodoList.Api.Models;
+
+namespace TodoList.Api.Tests.Controllers
+{
+    [TestFixture]
+    public class ItemsControllerTest
+    {
+        [Test]
+        public void Get__WithoutParams_ReturnsArrayOfItems()
+        {
+            var itemsController = new ItemsController();
+
+            var result = itemsController.Get();
+
+            Assert.That(result, Is.InstanceOf(typeof(IEnumerable<Item>)));
+            Assert.That(result, Is.Not.Empty);
+        }
+    }
+}
