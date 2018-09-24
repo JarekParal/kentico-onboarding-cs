6ae65de1eb199aa67abae7cba63a9308e920f5fa
JarekParal <jaroslavp@kentico.com>
2018-09-24 18:05:55 +0200

    KA-381 Convert all functions in ItemsController to arrow functions

--- TodoList/TodoList.Api/Controllers/ItemsController.cs
+++ TodoList/TodoList.Api/Controllers/ItemsController.cs
@@ -9,7 +9,8 @@ namespace TodoList.Api.Controllers
     [RoutePrefix("api/v1/Items")]
     public class ItemsController : ApiController
     {
-        private static readonly Item[] s_items = {
+        private static readonly Item[] s_items =
+        {
             new Item {Id = new Guid("1BBA61A3-9DA6-4A28-8A12-F543BB5EA737"), Text = "Dog"},
             new Item {Id = new Guid("BFA20109-F15E-4F5C-B395-2879E02BC422"), Text = "Cat"},
             new Item {Id = new Guid("4BAF698C-AF41-4AA1-8465-85C00073BD13"), Text = "Elephant"}
@@ -17,32 +18,22 @@ namespace TodoList.Api.Controllers
 
         [Route("")]
         public async Task<IHttpActionResult> GetAsync()
-        {
-            return Ok(await Task.FromResult(s_items));
-        }
+            => Ok(await Task.FromResult(s_items));
 
         [Route("{id}", Name = "GetItem")]
         public async Task<IHttpActionResult> GetAsync(Guid id)
-        {
-            return Ok(await Task.FromResult(s_items[0]));
-        }
+            => Ok(await Task.FromResult(s_items[0]));
 
         [Route("")]
         public async Task<IHttpActionResult> PostAsync([FromBody] Item item)
-        {
-            return Ok(await Task.FromResult(new Item {Id = item.Id, Text = item.Text}));
-        }
+            => Ok(await Task.FromResult(new Item {Id = item.Id, Text = item.Text}));
 
         [Route("{id}")]
         public async Task<IHttpActionResult> PutAsync(Guid id, [FromBody] Item item)
-        {
-            return StatusCode(await Task.FromResult(HttpStatusCode.OK));
-        }
+            => StatusCode(await Task.FromResult(HttpStatusCode.OK));
 
         [Route("{id}")]
         public async Task<IHttpActionResult> DeleteAsync(Guid id)
-        {
-            return StatusCode(await Task.FromResult(HttpStatusCode.NoContent));
-        }
+            => StatusCode(await Task.FromResult(HttpStatusCode.NoContent));
     }
 }
