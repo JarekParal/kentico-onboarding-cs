b1438e38325ff0ad2f323c796bcdae19d9a2e117
JarekParal <jaroslavp@kentico.com>
2018-10-01 11:48:29 +0200

    KA-381 Refactor ItemCompare - used ReSharper to generate compare

--- TodoList/TodoList.Api.Tests/Utils/ItemCompare.cs
+++ TodoList/TodoList.Api.Tests/Utils/ItemCompare.cs
@@ -5,18 +5,21 @@ namespace TodoList.Api.Tests.Utils
 {
     public class ItemCompare : IEqualityComparer<Item>
     {
-        public bool Equals(Item i1, Item i2)
+        public bool Equals(Item x, Item y)
         {
-            if (i1 == null || i2 == null)
-            {
-                return i1 == null && i2 == null;
-            }
-            return i1.Id.Equals(i2.Id) && i1.Text.Equals(i2.Text);
+            if (ReferenceEquals(x, y)) return true;
+            if (ReferenceEquals(x, null)) return false;
+            if (ReferenceEquals(y, null)) return false;
+            if (x.GetType() != y.GetType()) return false;
+            return x.Id.Equals(y.Id) && string.Equals(x.Text, y.Text);
         }
 
-        public int GetHashCode(Item item)
+        public int GetHashCode(Item obj)
         {
-            return (item.Id + item.Text).GetHashCode(); // TODO: check GetHashCode()
+            unchecked
+            {
+                return (obj.Id.GetHashCode() * 397) ^ (obj.Text != null ? obj.Text.GetHashCode() : 0);
+            }
         }
     }
 }
