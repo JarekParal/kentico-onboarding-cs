e23c8d9025385c374da68c58a39fa0d7354e645e
JarekParal <jaroslavp@kentico.com>
2018-10-01 08:36:40 +0200

    KA-381 Edit tests - add ItemCompare class and use this class in tests

--- TodoList/TodoList.Api.Tests/Controllers/ItemsControllerTest.cs
+++ TodoList/TodoList.Api.Tests/Controllers/ItemsControllerTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Threading;
@@ -7,6 +8,7 @@ using System.Web.Http;
 using NUnit.Framework;
 using TodoList.Api.Controllers;
 using TodoList.Api.Models;
+using TodoList.Api.Tests.Utils;
 
 namespace TodoList.Api.Tests.Controllers
 {
@@ -14,6 +16,16 @@ namespace TodoList.Api.Tests.Controllers
     public class ItemsControllerTest
     {
         private ItemsController _controller;
+        private ItemCompare _compare;
+
+        private static readonly Item[] s_items =
+        {
+            new Item {Id = new Guid("1BBA61A3-9DA6-4A28-8A12-F543BB5EA737"), Text = "Dog"},
+            new Item {Id = new Guid("BFA20109-F15E-4F5C-B395-2879E02BC422"), Text = "Cat"},
+            new Item {Id = new Guid("4BAF698C-AF41-4AA1-8465-85C00073BD13"), Text = "Elephant"}
+        };
+
+        private static readonly Item s_catdog = new Item { Id = new Guid("00000000-0000-0000-0000-000000000000"), Text = "CatDog" };
 
         [SetUp]
         public void SetUp()
@@ -23,6 +35,7 @@ namespace TodoList.Api.Tests.Controllers
                 Request = new HttpRequestMessage(),
                 Configuration = new HttpConfiguration()
             };
+            _compare = new ItemCompare();
         }
 
         [Test]
@@ -34,20 +47,19 @@ namespace TodoList.Api.Tests.Controllers
 
             Assert.That(contentResult.StatusCode, Is.EqualTo(HttpStatusCode.OK));
             Assert.That(items, Is.Not.Empty);
+            Assert.That(items, Is.EqualTo(s_items).Using(_compare));
         }
 
         [Test]
         public async Task GetAsync_WithValidId_ReturnsOneItem()
         {
-            var id = new Guid("1BBA61A3-9DA6-4A28-8A12-F543BB5EA737");
+            var id = s_items[0].Id;
 
             var actionResult = await _controller.GetAsync(id);
             var contentResult = actionResult.ExecuteAsync(CancellationToken.None).Result;
             contentResult.TryGetContentValue<Item>(out var item);
 
-            Assert.That(contentResult.StatusCode, Is.EqualTo(HttpStatusCode.OK));
-            Assert.That(item.Id, Is.EqualTo(id));
-            Assert.That(item.Text, Is.EqualTo("Dog"));
+            Assert.That(item, Is.EqualTo(s_items[0]).Using(_compare));
         }
 
         [Test]
@@ -70,13 +82,13 @@ namespace TodoList.Api.Tests.Controllers
             contentResult.TryGetContentValue<Item>(out var item);
 
             Assert.That(contentResult.StatusCode, Is.EqualTo(HttpStatusCode.OK));
-            Assert.That(item.Text, Is.EqualTo("CatDog"));
+            Assert.That(item, Is.EqualTo(s_catdog).Using(_compare));
         }
 
         [Test]
         public async Task PutAsync_EditExistingItem_ReturnsStatusCodeOk()
         {
-            var id = new Guid("1BBA61A3-9DA6-4A28-8A12-F543BB5EA737");
+            var id = s_items[0].Id;
 
             var actionResult = await _controller.PutAsync(id, new Item {Id = id, Text = "DogDog"});
             var contentResult = actionResult.ExecuteAsync(CancellationToken.None).Result;
@@ -99,7 +111,7 @@ namespace TodoList.Api.Tests.Controllers
         [Test]
         public async Task DeleteAsync_OneExistingItem_ReturnsStatusCodeNoContent()
         {
-            var id = new Guid("1BBA61A3-9DA6-4A28-8A12-F543BB5EA737");
+            var id = s_items[0].Id;
 
             var actionResult = await _controller.DeleteAsync(id);
             var contentResult = actionResult.ExecuteAsync(CancellationToken.None).Result;
---
+++ TodoList/TodoList.Api.Tests/Utils/ItemCompare.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using TodoList.Api.Models;
+
+namespace TodoList.Api.Tests.Utils
+{
+    public class ItemCompare : IEqualityComparer<Item>
+    {
+        public bool Equals(Item i1, Item i2)
+        {
+            if (i1 == null || i2 == null)
+            {
+                return i1 == null && i2 == null;
+            }
+            return i1.Id.Equals(i2.Id) && i1.Text.Equals(i2.Text);
+        }
+
+        public int GetHashCode(Item item)
+        {
+            return (item.Id + item.Text).GetHashCode(); // TODO: check GetHashCode()
+        }
+    }
+}
