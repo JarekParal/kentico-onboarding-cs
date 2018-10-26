e567d6c632d64b455ee3fa919f5fff6526664345
JarekParal <jaroslavp@kentico.com>
2018-10-26 17:58:25 +0200

    KA-382 Add parameter lifetimeEnum to RegisterType() in TodoListContainer
    Add enum ContainerLifetimeEnum. Add method GetLifetimeManager().
    Use in RegisterType() method.

---
+++ TodoList/Src/TodoList.Contracts/DI/ContainerLifetimeEnum.cs
@@ -0,0 +1,12 @@
+﻿namespace TodoList.Contracts.DI
+{
+    public enum ContainerLifetimeEnum
+    {
+        TransientLifetimeManager,
+        ContainerControlledLifetimeManager,
+        HierarchicalLifetimeManager,
+        PerResolveLifetimeManager,
+        PerThreadLifetimeManager,
+        ExternallyControlledLifetimeManager
+    }
+}
