6c24e84b8dd546f107f41e2bcb208e518ca5445d
JarekParal <jaroslavp@kentico.com>
2018-11-29 15:11:52 +0100

    KA-382 Add ItemUrlHelper(Test) and RouteNames
    - Fix potential problem with CreateAtRoute() by adding classes RouteNames, ItemUrlHelper
    and replace CreataAtRoute() by Create() where is safer way of generating the URL.
    - Add new project Api.Services with class ItemUrlHelper(Test).
    - Add interfaces ITodoListUrlHelper and IRouteNames to project Contracts.
    - Add class RouteNames to project Api.

---
+++ TodoList/Src/TodoList.Api.Services/ApiServicesBootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+using System.Web;
+using TodoList.Api.Services.UrlHelpers;
+using TodoList.Contracts.Api.Services;
+using TodoList.Contracts.DI;
+
+namespace TodoList.Api.Services
+{
+    public class ApiServicesBootstrapper : IBootstrapper
+    {
+        public ITodoListContainer Register(ITodoListContainer container)
+            => container
+                .RegisterType<ITodoListUrlHelper, ItemUrlHelper>(Lifetime.PerRequest)
+                .RegisterType<HttpRequestMessage>(Lifetime.PerRequest, GetMessage);
+
+        private static HttpRequestMessage GetMessage()
+            => HttpContext.Current.Items["MS_HttpRequestMessage"] as HttpRequestMessage;
+    }
+}
