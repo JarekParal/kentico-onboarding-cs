3d57aa6abe017b172f027eb3bbf1236de2601236
JarekParal <jaroslavp@kentico.com>
2018-11-29 15:28:06 +0100

    KA-382 Add DependencyResolverBuilder(Test)
    - Change the DependencyConfig, use here DependencyResolverBuilder class
    which implement the builder pattern for Bootsrappers.

---
+++ TodoList/Src/TodoList.DI/DependencyResolvers/DependencyResolverBuilder.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Http.Dependencies;
+using TodoList.Contracts.DI;
+using TodoList.DI.Containers;
+
+namespace TodoList.DI.DependencyResolvers
+{
+    public class DependencyResolverBuilder
+    {
+        private readonly ITodoListContainer _container;
+
+        public DependencyResolverBuilder() : this(new TodoListContainer())
+        {
+        }
+
+        internal DependencyResolverBuilder(ITodoListContainer container)
+        {
+            _container = container;
+        }
+
+        public DependencyResolverBuilder Bootstrap<TBootstrapper>()
+            where TBootstrapper : IBootstrapper, new()
+        {
+            IBootstrapper bootstrapper = new TBootstrapper();
+            bootstrapper.Register(_container);
+            return this;
+        }
+
+        public IDependencyResolver Build()
+        {
+            return new DependencyResolver(_container.GetProvider());
+        }
+    }
+}
