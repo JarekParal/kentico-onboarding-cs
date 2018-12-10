2a8a21b144e8462bb6130091b71008c8e7f259fb
JarekParal <jaroslavp@kentico.com>
2018-12-10 14:05:52 +0100

    fixup! fixup! fixup! KA-382 Add DependencyResolverBuilder(Test)

--- TodoList/Tests/TodoList.DI.Tests/DependencyResolvers/DependencyResolverBuilderTest.cs
+++ TodoList/Tests/TodoList.DI.Tests/DependencyResolvers/DependencyResolverBuilderTest.cs
@@ -8,27 +8,81 @@ namespace TodoList.DI.Tests.DependencyResolvers
     [TestFixture]
     public class DependencyResolverBuilderTest
     {
-        private class FakeBootstrapper : IBootstrapper
+        private class FakeFirstBootstrapper : IBootstrapper
         {
             public static int RegisterCallCounter { get; private set; }
+
+            public static void ResetRegisterCallCounter()
+            {
+                RegisterCallCounter = 0;
+            }
 
             public ITodoListContainer Register(ITodoListContainer container)
             {
                 ++RegisterCallCounter;
                 return container;
             }
+        }
 
+        private class FakeSecondBootstrapper : IBootstrapper
+        {
+            public static int RegisterCallCounter { get; private set; }
+
+            public static void ResetRegisterCallCounter()
+            {
+                RegisterCallCounter = 0;
+            }
+
+            public ITodoListContainer Register(ITodoListContainer container)
+            {
+                ++RegisterCallCounter;
+                return container;
+            }
+        }
+
+        [SetUp]
+        public void InitFakeBootstreppers()
+        {
+            FakeFirstBootstrapper.ResetRegisterCallCounter();
+            FakeSecondBootstrapper.ResetRegisterCallCounter();
         }
 
         [Test]
-        public void Bootsrap_CheckCallBootsrapperRegisterMethod()
+        public void Bootsrap_CheckCallForOneBootsrapperRegisterMethod()
         {
             var container = Substitute.For<ITodoListContainer>();
             var builder = new DependencyResolverBuilder(container);
 
-            builder.Bootstrap<FakeBootstrapper>();
+            builder.Bootstrap<FakeFirstBootstrapper>();
 
-            Assert.That(FakeBootstrapper.RegisterCallCounter, Is.EqualTo(1));
+            Assert.That(FakeFirstBootstrapper.RegisterCallCounter, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Bootsrap_CheckCallForTwoBootsrapperRegisterMethod()
+        {
+            var container = Substitute.For<ITodoListContainer>();
+            var builder = new DependencyResolverBuilder(container);
+
+            builder.Bootstrap<FakeFirstBootstrapper>();
+            builder.Bootstrap<FakeSecondBootstrapper>();
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(FakeFirstBootstrapper.RegisterCallCounter, Is.EqualTo(1));
+                Assert.That(FakeSecondBootstrapper.RegisterCallCounter, Is.EqualTo(1));
+            });
+        }
+
+        [Test]
+        public void Build_FakeBootstrapper_NotNull()
+        {
+            var builder = new DependencyResolverBuilder();
+            builder.Bootstrap<FakeFirstBootstrapper>();
+
+            var result = builder.Build();
+
+            Assert.That(result, Is.Not.Null);
         }
     }
 }
