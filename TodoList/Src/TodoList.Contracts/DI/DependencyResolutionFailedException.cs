b1fa88b220a27085a244f062b8f6b8f36cbad0a3
JarekParal <jaroslavp@kentico.com>
2018-12-10 11:03:14 +0100

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! KA-382 Encapsulate Unity

--- TodoList/Src/TodoList.Contracts/DI/DependencyResolutionFailedException.cs
+++ TodoList/Src/TodoList.Contracts/DI/DependencyResolutionFailedException.cs
@@ -5,14 +5,6 @@ namespace TodoList.Contracts.DI
 {
     public class DependencyResolutionFailedException : Exception
     {
-        public DependencyResolutionFailedException()
-        {
-        }
-
-        public DependencyResolutionFailedException(string message) : base(message)
-        {
-        }
-
         public DependencyResolutionFailedException(string message, Exception innerException) : base(message,
             innerException)
         {
